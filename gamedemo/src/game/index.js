b33a25c1597bbc1dae0548ecfc557a4fe97e973f
xqcode <50690946+xqcode@users.noreply.github.com>
2019-06-17 17:27:38 +0800

    Add files via upload

---
+++ gamedemo/src/game/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../../public/game.css'
+import Game from './Game'
+
+
+ReactDOM.render(<Game />, document.getElementById('root'));
